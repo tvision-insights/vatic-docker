a44a5f7a9bd8fbd6da5992b46c6b6da0b82aa8e5
Julio Lajara <julio@tvisioninsights.com>
2018-05-02 15:49:50 -0400

    Fix missing shebang.

--- scripts/start_and_block.sh
+++ scripts/start_and_block.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 # This script starts up the webserver and waits indefinately. It's used
 # for testing and must be manually terminated.
 
 ./root/vatic/startup.sh
+
 while true; do sleep 86400; done
