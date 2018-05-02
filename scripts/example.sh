9c980e08aec08cc33b1447e0e062729c1f63af08
Julio Lajara <julio@tvisioninsights.com>
2018-05-02 14:01:03 -0400

    Fix copy paste issue.

--- scripts/example.sh
+++ scripts/example.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-ID=$1
-TODOVIDEOPATH=/root/vatic/data/videos_in/$ID
-DONEVIDEOPATH=/root/vatic/data/videos_out/$ID
-FRAMEPATH="/root/vatic/data/"$ID"_frames_in"
-
-mkdir -p $FRAMEPATH
-mkdir -p $DONEVIDEOPATH
-
-cd /root/vatic
-for i in $( ls $TODOVIDEOPATH); do
-    turkic extract $TODOVIDEOPATH/$i $FRAMEPATH --width 720 --height 480
-    mv $TODOVIDEOPATH/$i $DONEVIDEOPATH/
-done
-root@8cc6fdfc5066:~/vatic# cat example.sh 
-#!/bin/bash
-
 # Settings
 ID=$1
 VIDEOPATH=/root/vatic/data/videos_in/$ID
